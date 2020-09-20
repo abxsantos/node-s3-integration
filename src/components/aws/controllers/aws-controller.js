19ca85c5e57cfdc611185fb96cb1c31179fdcc41
Alexandre Xavier <ale.bxsantos@gmail.com>
2020-09-20 16:55:33 -0300

    refactor: Added class controller for AWS get presigned url

---
+++ src/components/aws/controllers/aws-controller.js
@@ -0,0 +1,34 @@
+import AWS from 'aws-sdk';
+
+class AWSController {
+  static async getPresignedUrl(req, res) {
+    const fileurls = [];
+    const s3 = new AWS.S3({
+      accessKeyId: process.env.TOKEN,
+      secretAccessKey: process.env.AMAZON_SECRET_KEY,
+    });
+    const awsParameters = {
+      Bucket: process.env.BUCKET,
+      Key: 'new_file.jpg',
+      Expires: 60 * 60,
+      ACL: 'bucket-owner-full-control',
+    };
+
+    s3.getSignedUrl('putObject', awsParameters, function (err, url) {
+      if (err) {
+        console.log('wow! something went wrong here');
+        res.json({ msg: 'hmm theres an error' });
+      } else {
+        fileurls[0] = url;
+        console.log(`Presigned URL: ${this.fileurls}`);
+        res.json({
+          success: true,
+          message: 'AWS SDK S3 Pre-signed urls generated successfully.',
+          urls: fileurls,
+        });
+      }
+    });
+  }
+}
+
+export default AWSController;
--- src/components/aws/routes/routes-aws.js
+++ src/components/aws/routes/routes-aws.js
@@ -1,39 +1,8 @@
 import { Router } from 'express';
-const aws = require('aws-sdk');
+import AWSController from '../controllers/aws-controller';
 
 const awsRouter = Router();
 
-awsRouter.get('/upload-to-s3', function (req, res) {
-  const fileurls = [];
-
-  const s3 = new aws.S3({
-    accessKeyId: process.env.TOKEN,
-    secretAccessKey: process.env.AMAZON_SECRET_KEY,
-  });
-
-  const file = 'new_file.jpg';
-  const expirationURLs = 60 * 60;
-  const parameters = {
-    Bucket: process.env.BUCKET,
-    Key: file,
-    Expires: expirationURLs,
-    ACL: 'bucket-owner-full-control',
-  };
-  s3.getSignedUrl('putObject', parameters, function (err, url) {
-    if (err) {
-      console.log(err);
-      console.log('wow! something went wrong here');
-      res.json({ msg: 'hmm theres an error' });
-    } else {
-      fileurls[0] = url;
-      console.log(`Presigned URL: ${fileurls}`);
-      res.json({
-        success: true,
-        message: 'AWS SDK S3 Pre-signed urls generated successfully.',
-        urls: fileurls,
-      });
-    }
-  });
-});
+awsRouter.get('/upload-to-s3', AWSController.getPresignedUrl);
 
 export default awsRouter;
