30878cf8ffff565770e3d4fc7278493727b611c6
Alexandre Xavier <ale.bxsantos@gmail.com>
2020-09-20 20:40:32 -0300

    features: Added errors for environment variables not being set

--- src/index.js
+++ src/index.js
@@ -2,8 +2,25 @@ import server from './app';
 
 require('dotenv').config();
 
+if (!process.env.TOKEN) {
+  throw new Error('TOKEN environment variable must be defined');
+}
+if (!process.env.AMAZON_SECRET_KEY) {
+  throw new Error('AMAZON_SECRET_KEY environment variable must be defined');
+}
+if (!process.env.BUCKET) {
+  throw new Error('BUCKET environment variable must be defined');
+}
+if (!process.env.REGION) {
+  throw new Error('REGION environment variable must be defined');
+}
+if (!process.env.KEY) {
+  throw new Error('KEY environment variable must be defined');
+}
+
 const PORT = process.env.PORT || 8000;
 
 server.listen(PORT, () => {
-  console.log(`hello world, this port is ${PORT}`);
+  // eslint-disable-next-line no-console
+  console.log(`hello world, this server is using port: ${PORT}`);
 });
